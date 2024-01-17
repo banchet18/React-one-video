8bafb266c100234457a1a3a51051cefd8fa1011e
banchet18 <banjarac6@gmail.com>
2024-01-17 19:09:32 +0530

    c

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,8 +1,14 @@
 const a = 400;
+
+const Myheading = () => {
+  return <h1>Myheading</h1>;
+};
+
 const App = () => {
   const a = 200;
   return (
     <div>
+      <Myheading />
       <h1>Welcome to React {a}</h1>
       <p></p>
     </div>
