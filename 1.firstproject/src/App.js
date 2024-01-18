2ca459e7f09ccb0ea831a60866e26e0c98f20582
banchet18 <banjarac6@gmail.com>
2024-01-18 10:40:18 +0530

    c

--- 1.firstproject/src/App.js
+++ 1.firstproject/src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
+import "./styles/App.scss";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="" element />
       </Routes>
